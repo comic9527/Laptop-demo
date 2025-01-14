c02c093e8da8025252deeaacda3fa3eb61028337
Laptop Song <394028461@qq.com>
2025-01-14 12:33:16 +0800

    question genner

---
+++ newscript.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         第一次尝试
+// @namespace    http://tampermonkey.net/
+// @version      2025-01-14
+// @description  你好世界
+// @author       bysong
+// @license      AGPL-3.0
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log('你的名字是什么？')
+})();
